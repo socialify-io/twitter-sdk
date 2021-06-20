0505dfc97c7c558d550f18e2585a4666d5a119ad
Pengwius <tomekftomekf@gmail.com>
2021-06-20 21:49:54 +0200

    Add comments to request helper

--- Sources/TwitterSdk/Helpers/requestHelpers.swift
+++ Sources/TwitterSdk/Helpers/requestHelpers.swift
@@ -13,8 +13,14 @@ extension TwitterClient {
     /// MARK: - Request helpers
     func makeRequest(request: URLRequest, completion: @escaping (Result<[String: Any], TwitterError>) -> Void) {
         var request = request
+        
+        // Adding User-Agent
         request.addValue("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36", forHTTPHeaderField: "User-Agent")
+        
+        // Sending request
         session?.dataTask(with: request) { (data, response, error) in
+            
+            // Looking for errors
             guard let data = data else {
                   if let _ = error {
                     completion(.failure(TwitterError.ConnectionError))
@@ -24,6 +30,7 @@ extension TwitterClient {
                 return
                 }
             
+            // Preparing return data
             let returnBody: [String: Any] = [
                 "data": data,
                 "response": response
